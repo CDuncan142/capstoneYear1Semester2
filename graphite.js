74016b64348cbad169827cc24a9166c38577adb5
Conner Duncan <CDuncan142@west-mec.org>
2018-05-17 18:14:48 -0700

    Final Commit
    MERCH

---
+++ graphite.js
@@ -0,0 +1,17 @@
+/*
+Graphite.js
+author: Conner Duncan
+*/
+
+/* Global variable */
+
+var rotations = Array("rotate(22.5deg)", "rotate(-22.5deg)", "rotate(45deg)", "rotate(-45deg)");
+
+/* Index */
+
+function wiggle() {
+    setInterval
+    document.getElementById("logo").style.transform = rotations[i];
+
+}
+}
