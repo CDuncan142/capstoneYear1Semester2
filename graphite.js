57153fc3438c8e85bce27eeb5a4623228e0012e0
CDuncan142 <31226648+CDuncan142@users.noreply.github.com>
2018-05-17 22:37:27 -0700

    javascript

--- graphite.js
+++ graphite.js
@@ -5,13 +5,26 @@ author: Conner Duncan
 
 /* Global variable */
 
-var rotations = Array("rotate(22.5deg)", "rotate(-22.5deg)", "rotate(45deg)", "rotate(-45deg)");
+var rotations;
+var timers;
+var timer1, timer2, timer3, timer4;
+var clickTimer;
+var endClickTimer, clearClickTimer;
+var angle = 1;
+var slow = 0.9;
 
 /* Index */
 
-function wiggle() {
-    setInterval
-    document.getElementById("logo").style.transform = rotations[i];
-
-}
+function wiggle(fun) {
+  if (fun==false) {
+  rotations = Array("rotate(10deg)", "rotate(-2.5deg)", "rotate(5deg)", "rotate(-10deg)", "rotate(0deg)");
+  timer1 = setTimeout(function(){document.getElementById("logo").style.transform = rotations[0];}, (1 * 0300));
+  timer2 = setTimeout(function(){document.getElementById("logo").style.transform = rotations[3];}, (2 * 0300));
+  timer3 = setTimeout(function(){document.getElementById("logo").style.transform = rotations[2];}, (3 * 0300));
+  timer4 = setTimeout(function(){document.getElementById("logo").style.transform = rotations[1];}, (4 * 0300));
+  timer5 = setTimeout(function(){document.getElementById("logo").style.transform = rotations[4];}, (5 * 0300));
+  }else {
+    clickTimer = setInterval(function(){angle+=angle/60; document.getElementById("logo").style.transform = "rotate("+angle+"deg)";}, (0025));
+    endClickTimer = setInterval(function(){clearClickTimer = clearInterval(clickTimer); angle=slow; slow*=slow;}, 10000);
+  }
 }
